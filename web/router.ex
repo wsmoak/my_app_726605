ef5b82a1032ead15e81cb23fb3f580f980ed0e4a
Wendy Smoak <wsmoak@gmail.com>
2015-07-26 18:15:27 -0400

    Add users resources to browser scope

--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,7 @@ defmodule MyApp_726605.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
