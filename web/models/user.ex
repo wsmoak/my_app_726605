c2fea2e7cf5d28ffbd9380e0b3ca6cd98531a24a
Wendy Smoak <wsmoak@gmail.com>
2015-07-26 21:56:15 -0400

    Add new fields to model. Make all fields optional. Add user_id to show template.

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,12 +4,16 @@ defmodule MyApp_726605.User do
   schema "users" do
     field :name, :string
     field :email, :string
+    field :user_id, :string
+    field :access_token, :binary
+    field :access_token_expires_at, Ecto.DateTime
+    field :refresh_token, :binary
 
     timestamps
   end
 
-  @required_fields ~w(name email)
-  @optional_fields ~w()
+ @required_fields ~w()
+ @optional_fields ~w(name email user_id access_token access_token_expires_at refresh_token)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
