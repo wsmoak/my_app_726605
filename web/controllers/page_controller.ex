0c7fbc6c55543303a57524d81fe1aa2fbf4cf4fd
Wendy Smoak <wsmoak@gmail.com>
2015-07-27 07:49:16 -0400

    Simulate adding a user to the database after the OAuth flow

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,24 @@
 defmodule MyApp_726605.PageController do
   use MyApp_726605.Web, :controller
 
+  alias MyApp_726605.User
+
+  def index(conn, %{"test" => _}) do
+    changeset = User.changeset(%User{},
+      %{name: "Amy Smith",
+        email: "amy@example.com",
+        user_id: "ABC123",
+        access_token: "fjlsfj2l34h2lh2l432lj",
+        refresh_token: "l4l2k34h2l234k2h97sf",
+        access_token_expires_at: {{2015, 12, 31}, {12, 00, 00}}
+      })
+    Repo.insert!(changeset)
+
+    render conn, "index.html"
+  end
+
   def index(conn, _params) do
     render conn, "index.html"
   end
+
 end
