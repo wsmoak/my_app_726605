d34a07cab159817db7f789c6c6cf26028d68c1f8
Wendy Smoak <wsmoak@gmail.com>
2015-07-26 20:18:29 -0400

    Update add_fields_to_users migration

--- priv/repo/migrations/20150727000247_add_fields_to_users.exs
+++ priv/repo/migrations/20150727000247_add_fields_to_users.exs
@@ -2,5 +2,11 @@ defmodule MyApp_726605.Repo.Migrations.AddFieldsToUsers do
   use Ecto.Migration
 
   def change do
+    alter table(:users) do
+      add :user_id, :string
+      add :access_token, :binary
+      add :access_token_expires_at, :datetime
+      add :refresh_token, :binary
+    end
   end
 end
